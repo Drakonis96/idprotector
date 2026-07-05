e5c47d3a34956d71f2bb26656b4911d2793a2602
Drakonis96 <41027003+Drakonis96@users.noreply.github.com>
2026-07-05 19:47:54 +0200

    Bug fixes and improvements

--- public/js/watermark.js
+++ public/js/watermark.js
@@ -7,7 +7,7 @@
   var SL = global.SL || (global.SL = {});
 
   SL.BRAND = "IDprotector";
-  SL.VERSION = "v0.1.7";
+  SL.VERSION = "v0.1.8";
 
   // Available patterns (id + human label). Order defines UI order.
   SL.PATTERNS = [
